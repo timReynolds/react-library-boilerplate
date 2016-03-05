4d040c38a5ab510c57641de926a4b8f4115227df
Tim Reynolds <tim@timothyreynolds.co.uk>
2016-03-05 15:38:15 +0000

    Updated to lastest dependencies and included dependencies status in read me.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ export default class Counter extends Component {
 
     this.state = { count: 0 };
     this.update = this.update.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
   update(value) {
     this.setState({ count: this.state.count + value });
+  }
+  increment() {
+    return this.update(1);
+  }
+  decrement() {
+    return this.update(-1);
   }
   render() {
     return (
         <div>
           <p>{this.state.count}</p>
-          <button onClick={function inc() {this.update(1);}}>+1</button>
-          <button onClick={function dec() {this.update(-1);}}>-1</button>
+          <button onClick={this.increment}>+1</button>
+          <button onClick={this.decrement}>-1</button>
         </div>
       );
   }
